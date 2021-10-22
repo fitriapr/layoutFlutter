90cb4ffa7e47215684adc15af88a08c7f8976b8f
fitriapr <fitriapriyanitugas@gmail.com>
2021-10-23 00:35:37 +0700

    Latihan Layout Flutter

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Flutter")),
+        body: Container(
+          color: Colors.black26,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                height: 200,
+                color: Colors.yellow,
+              ),
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                child: Text(
+                  "Fitri Apriyani - STTBANDUNG",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                        child: Container(
+                      color: Colors.orange,
+                      height: 150,
+                    )),
+                    Expanded(child: Container(color: Colors.green)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
